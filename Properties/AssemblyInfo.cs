58e82e6c4875c0581d8763fcf10a6d41f2c3a8fe
DeanZhouLin <funny_zhoulin@163.com>
2014-01-10 11:20:04 +0800

    EntityFramework.Extended Init

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("聚好")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("642461ab-b078-4b69-a0ca-4d34bbbc6113")]
+[assembly: InternalsVisibleTo("EntityFramework.Extended.Test")]
+[assembly: AssemblyTitleAttribute("EntityFramework.Extended")]
+[assembly: AssemblyDescriptionAttribute("EntityFramework.Extended")]
+[assembly: AssemblyCompanyAttribute("Jufine")]
+[assembly: AssemblyProductAttribute("EntityFramework.Extended")]
+[assembly: AssemblyCopyrightAttribute("聚好信息")]
+[assembly: AssemblyVersionAttribute("1.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
